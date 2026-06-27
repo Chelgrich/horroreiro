9486805ae67cc4fecd158d821b818ae95a0ffcee
Chelgrich <chelgrich@gmail.com>
2026-06-28 00:11:17 +0300

    Improve range slider contrast

--- boot-loader.js
+++ boot-loader.js
@@ -15,7 +15,7 @@
     const localDevEnv = {
       SUPABASE_URL: 'https://uogzcozbnosfguyfbvhe.supabase.co',
       SUPABASE_ANON_KEY: 'sb_publishable_trS_-TlQwFqcM59nELIdsw_ygVI6B0j',
-      APP_BUILD_VERSION: 'dev-local-16'
+      APP_BUILD_VERSION: 'dev-local-17'
     };
 
     function getVersionedAssetUrl(src, buildVersion) {
